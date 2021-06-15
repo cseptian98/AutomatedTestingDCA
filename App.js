91c3404c8028f013336a7d3a325c55a3b5bedc16
Chandra Septian <cst_dev155@unitedtractors.com>
2021-06-15 09:12:23 +0700

    Initial Commit

---
+++ App.js
@@ -0,0 +1,101 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-shadow */
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('./src/assets/images/tractors.png')}
+      />
+
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          onChangeText={email => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          secureTextEntry={true}
+          onChangeText={password => setPassword(password)}
+        />
+      </View>
+
+      <TouchableOpacity>
+        <Text style={styles.forgot_button}>Forgot Password?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginBtn}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 30,
+  },
+
+  inputView: {
+    backgroundColor: '#FFFF00',
+    borderRadius: 30,
+    width: '70%',
+    height: 45,
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+  },
+
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+
+  loginBtn: {
+    width: '80%',
+    borderRadius: 25,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+    backgroundColor: '#FFAB00',
+  },
+});
+
+export default App;
