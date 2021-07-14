4de6ccbbe9fe0c564ac90033f0a941f480babc22
Chandra Septian <cst_dev155@unitedtractors.com>
2021-07-14 23:26:20 +0700

    Split navigator

--- App.js
+++ App.js
@@ -1,27 +1,12 @@
 /* eslint-disable prettier/prettier */
 import React from 'react'
-
 import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
-
-import Login from './src/views/Login'
-import Home from './src/views/Home'
-import Register from './src/views/Register'
-
-const Stack = createStackNavigator()
+import AppNavigator from 'views/Navigator/AppNavigator'
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{title: 'Automated Testing'}}
-        />
-        <Stack.Screen name="Register" component={Register} />
-      </Stack.Navigator>
+      <AppNavigator />
     </NavigationContainer>
   )
 }
